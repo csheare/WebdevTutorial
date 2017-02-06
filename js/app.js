b2146e3d0c4fbcae427c2d00c1c80fba35a38e69
Alexander Six <alexanderhsix@gmail.com>
2017-02-06 18:45:25 -0500

    Main page skeleton completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+
+$(document).ready(function() {
+  $('#alex').on('click', function() {
+    window.location.href = "alex.html";
+  });
+
+  $('#courtney').on('click', function() {
+    window.location.href = "courtney/index.html";
+  });
+
+  $('#isaiah').on('click', function() {
+    window.location.href = "isaiah/index.html";
+  });
+
+});
